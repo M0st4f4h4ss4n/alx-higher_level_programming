01d9951be0e1a2569b2bcbf27c7398f786ce40b8
Mostafa Hasan <m.hasan@odak-eg.com>
2023-01-10 20:14:40 +0000

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""
-contains the MyList class
-"""
-
 
 class MyList(list):
-    """a subclass of list"""
+    """
+    MyList is a class that inherits from the built-in list class in Python,
+    and also has a method `print_sorted` that prints the list in ascending order
+    """
     def __init__(self):
-        """initializes the object"""
+        """
+        initialize the object, call the parent's class __init__
+        """
         super().__init__()
-
+        
     def print_sorted(self):
-        """prints the sorted list"""
+        """
+        This method prints the sorted version of the MyList
+        It does not change the original unsorted list
+        """
         print(sorted(self))
+
