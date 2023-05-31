e286f429d228fb277773b2e07a6a26ba838cdc2a
Mostafa Hassan <m0st4f4h4ss4n@odak-eg.com>
2023-05-31 22:53:53 +0300

    SQL

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,10 +1,9 @@
--- List all the cities of california found in the database.
- -- Results are ordered by ascending cities.id.
+-- Lists all cities of CA in the database hbtn_0d_usa.
+-- Results are ordered by ascending cities.id.
 SELECT `id`, `name`
-FROM `cities`
-WHERE `state_id` IN (
-	SELECT `id`
-	FROM `states`
-	WHERE `name` = "California"
-)
-ORDER BY `id`;
+  FROM `cities`
+ WHERE `state_id` IN
+       (SELECT `id`
+	  FROM `states`
+	 WHERE `name` = "California")
+ ORDER BY `id`;
